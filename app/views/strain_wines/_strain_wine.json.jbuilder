6fc2e3fc53e9b6b28c79227ba073ef469bea0f5a
Psicomago <carlosaravena.coach@gmail.com>
2022-07-29 21:52:04 -0400

    first commit

---
+++ app/views/strain_wines/_strain_wine.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! strain_wine, :id, :strain_id, :wine_id, :strain_id, :wine_id, :percentage, :created_at, :updated_at
+json.url strain_wine_url(strain_wine, format: :json)
---
+++ db/migrate/20220730011349_create_strain_wines.rb
@@ -0,0 +1,13 @@
+class CreateStrainWines < ActiveRecord::Migration[5.2]
+  def change
+    create_table :strain_wines do |t|
+      t.references :strain, foreign_key: true
+      t.references :wine, foreign_key: true
+      t.string :strain_id
+      t.string :wine_id
+      t.integer :percentage
+
+      t.timestamps
+    end
+  end
+end
